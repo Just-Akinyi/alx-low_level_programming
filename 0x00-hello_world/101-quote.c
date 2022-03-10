78de17a0c683fb92a2772ea3363a5733083fc732
root <justakinyi1@gmail.com>
2022-03-10 13:56:15 -0800

    l

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main - prints the phrase "and that piece of art is useful" "- Dora Korpar, 2015-10-19
- * Return: 1 if exited properly
+ * main - Prints out the last part of a quote in the standard error.
+ * Return: 1 if success.
  */
 int main(void)
 {
-	puts("and that piece of art is useful"\" - Dora Korpar, 2015-10-19");
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
