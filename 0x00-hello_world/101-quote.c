cccf409c1ad0a46715b0598eae64be356cd8d5ff
root <justakinyi1@gmail.com>
2022-03-10 13:35:36 -0800

    k

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints the phrase "and that piece of art is useful" "- Dora Korpar, 2015-10-19
+ * Return: 1 if exited properly
+ */
+int main(void)
+{
+	puts("and that piece of art is useful"\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
