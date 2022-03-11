4c0679b7ed3f49170db20ce0dea29299035b41f8
root <justakinyi1@gmail.com>
2022-03-11 15:25:53 -0800

    l

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,12 +12,12 @@ Chukwuma Daniel00:51
  */
 int main(void)
 {
-        int i, j;
+	int i, j;
 
-        for (i = 97; i < 123; i++)
-                putchar(i);
-        for (j = 65; j < 91; j++)
-                putchar(j);
-        putchar(10);
-        return (0);
+	for (i = 97; i < 123; i++)
+		putchar(i);
+	for (j = 65; j < 91; j++)
+		putchar(j);
+	putchar(10);
+	return (0);
 }
