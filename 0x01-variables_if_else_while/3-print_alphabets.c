bc24e2d731e7e5cf08474f65cfed48f07403b536
root <justakinyi1@gmail.com>
2022-03-11 14:27:23 -0800

    k

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-        char c;
+	char c;
 
-        for (c = 'a'; c <= 'z'; ++c)
+	for (c = 'a'; c <= 'z'; ++c)
 		printf("%c", c);
 	for (c = 'A'; c <= 'Z'; ++c)
-                printf("%c", c);
-        printf("\n");
-        return (0);
+		printf("%c", c);
+	printf("\n");
+	return (0);
 }
