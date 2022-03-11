3eb171690528ab26952200bcc5bc7219d7b00751
root <justakinyi1@gmail.com>
2022-03-11 13:17:17 -0800

    k

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,14 +2,7 @@
 /**
  *displays alphabets
  */
-int main(void)
-/**
-* main - outputs alphabets from "a" to "z"
-*
-* @argument: it's an integer
-*
-* Return: 0 for success
-*/
+int main(void):q
 {
 	char c;
 	for (c = 'a'; c <= 'z'; ++c)
