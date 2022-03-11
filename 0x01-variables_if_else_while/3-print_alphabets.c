6c8e5ddf7ef7ebfd856c91733359961d072b4c33
root <justakinyi1@gmail.com>
2022-03-11 13:54:11 -0800

    k

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{
+	int ch;
+        /* print uppercase letters */
+
+	for (ch= ‘a’; ch<= ‘z’; ch++)
+             /* print lowercase letters */
+	for (ch= ‘A’; ch<= ‘Z’; cputchar(ch);
+			putchar("%c,"ch);
+			putchar("\n");
+	return(0);
+
+}
