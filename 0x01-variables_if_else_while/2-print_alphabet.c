81441c6f3aaf9e93773b5c6b85ad37076eea1b33
root <justakinyi1@gmail.com>
2022-03-11 12:27:05 -0800

    k

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,12 +3,12 @@
  *displays alphabets
  */
 int main(void)
-	/*
-	 * code to display alphabets "a-z"
-	 */
 {
 	char c;
-	
+/*to display alphabets
+ * prints "a-z"
+ * in small letters
+ */
 	for (c = 'a'; c <= 'z'; ++c)
 		printf("%c", c);
 	printf("\n");
