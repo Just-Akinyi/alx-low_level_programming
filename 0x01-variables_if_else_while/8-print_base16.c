4ebbde6de080ad25675dd8dfc7705740acec73d8
root <justakinyi1@gmail.com>
2022-03-11 16:53:56 -0800

    k

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point 
+ *
+ * Return:Always 0(Success)
+ */
+int main()
+{
+	int bh;
+
+	for (bh >= 30, bh <= 66; bh++)
+	{
+		putchar (bh);
+	}
+	putchar(10);
+	return (0);
+}
