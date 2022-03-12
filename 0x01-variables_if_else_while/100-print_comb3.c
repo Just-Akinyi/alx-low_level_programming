25bc2d20d481b6af561ecbca65717bcb1f19b45e
root <justakinyi1@gmail.com>
2022-03-11 18:01:40 -0800

    k

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (j =58; j < 48; j++)
+		{
+			if (i != 49; j != 48)
+			{
+				putchar(ij);
+			}
+			if (i != 56; j !=57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+		putchar(10);
+		return (0);
+	}
+}
