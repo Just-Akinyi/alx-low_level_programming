b99701f7de3e88a2502799df77aad1070e2241ee
root <justakinyi1@gmail.com>
2022-03-13 04:32:06 -0700

    l

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	int y, z;
+
+	for (y=48; y < 58; y++)
+	{
+		for (z = i+i; z < 58; z++)
+		{
+			putchar(y);
+			putchar(z);
+			if (y != 57 || z != 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar(10);
+	return (0);
