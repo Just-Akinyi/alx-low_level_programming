2d52f9c9acd9d326f5b53918c073263b74074190
root <justakinyi1@gmail.com>
2022-03-13 03:32:08 -0700

    l

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,15 +11,15 @@ int main(void)
 	for (i = 48; i < 58; i++)
 	{
 		for (j = i + 1; j < 58; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i < 56 || j < 57)
 			{
-				putchar(i);
-				putchar(j);
-				if (i != 56 && j != 57)
-				{
-					putchar(44);
-					putchar(32);
-				}
+				putchar(44);
+				putchar(32);
 			}
+		}
 	}
 	putchar(10);
 	return (0);
