84bfc676d551b1cd0ae8610bfbd6c1d3ef838a8b
root <justakinyi1@gmail.com>
2022-03-13 05:12:41 -0700

    l

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,25 +8,23 @@ int main(void)
 {
 	int v, w, y, z;
 
-	for (v = 48; )
+	int v = 48
+		putchar(v);
+	int w =48
 	{
-		for (w = 48; )
+		putchar(w);
+		putchar(32);
+	}
+	for (y = 48; y < 58; y++)
+	{
+		for (z = 48; z < 58; z++)
 		{
-			for (y = 48; y < 58; y++)
+			putchar(y);
+			putchar(z);
+			if (y != 57 || z != 57)
 			{
-				for (z = 48; z < 58; z++)
-				{
-					putchar(v);
-					putchar(w);
-					putchar(32);
-					putchar(y);
-					putchar(z);
-					if (y != 57 || z != 57)
-					{
-						putchar(44);
-						putchar(32);
-					}
-				}
+				putchar(44);
+				putchar(32);
 			}
 		}
 	}
