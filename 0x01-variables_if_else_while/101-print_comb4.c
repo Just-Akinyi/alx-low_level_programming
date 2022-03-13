b15e6dd08dd9c53213d9c48d34e60ef8a17fd005
root <justakinyi1@gmail.com>
2022-03-13 04:12:53 -0700

    l

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,7 +2,7 @@
 /**
  *main - Enters the program
  *
- * Retun - Always 0(Success)
+ * Retun - Always 0 (Success)
  */
 int main(void)
 {
@@ -17,7 +17,7 @@ int main(void)
 				putchar(i);
 				putchar(j);
 				putchar(k);
-				if (i < 56 || j < 57 || k < 58)
+				if (i != 55 || j != 56 || k != 57)
 				{
 					putchar(44);
 					putchar(32);
