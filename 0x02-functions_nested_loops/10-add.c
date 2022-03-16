5fa4f4b39a9b9a0fd8ee7fdf7d956074eb02c180
root <justakinyi1@gmail.com>
2022-03-16 13:44:37 -0700

    d

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * add - Adds two values
+  * @n1: The first number to be computed
+  * @n2: The second number to be computed
+  *
+  * Return: Integer value
+  */
+int add(int sum)
+{
+	int i;
+	int j;
+	int sum;
+
+	for(sum = i + j)
+	return (sum);
+}
