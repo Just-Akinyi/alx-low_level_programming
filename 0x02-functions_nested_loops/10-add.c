e22be77fe382b0ef284ed6439e8933ef815c3d85
root <justakinyi1@gmail.com>
2022-03-16 13:52:39 -0700

    d

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,12 +2,12 @@
 
 /**
   * add - Adds two values
-  * @n1: The first number to be computed
-  * @n2: The second number to be computed
+  * @i: The first number to be computed
+  * @j: The second number to be computed
   *
   * Return: Integer value
   */
 int add(int i, int j)
 {
-	return (int i + int j);
+	return (i + j);
 }
