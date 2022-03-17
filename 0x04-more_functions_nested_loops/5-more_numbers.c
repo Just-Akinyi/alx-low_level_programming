8f9a7642e7f8c405d3e974bd3e8dc023db702ed9
root <justakinyi1@gmail.com>
2022-03-17 13:53:34 -0700

    h

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main"
+
+/**
+ * more_numbers - prints 10 times the numbers from 0 to 14
+ *
+ * @n: is the number of times to print
+ * @x: is values to be printed
+ */
+void more_numbers(void);
+{
+	int n =10;
+
+	for (x = 0; x < 15; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
