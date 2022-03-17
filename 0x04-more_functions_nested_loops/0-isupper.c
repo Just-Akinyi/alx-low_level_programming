479addf0f98a45e2741210f1aeecb11cc6f3eee6
root <justakinyi1@gmail.com>
2022-03-17 09:30:01 -0700

    d

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main"
+
+/**
+ * isupper - checks for uppercase character
+ *
+ * Return: 1 if c is uppercase 0 otherwise
+ */
+int isupper(int c)
+{
+	int c;
+
+	for (c >= 65 && c < 91);
+	{
+		return (1);
+	}
+	return (0);
+}
