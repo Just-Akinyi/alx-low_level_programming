b82b5b0b079ab041a060b1319503282e7e9853db
Justin <justakinyi1@gmail.com>
2022-03-22 02:38:53 +0200

    j

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * puts - Prints a string
+ * @str: argument str to print
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	puts(str);
+}
