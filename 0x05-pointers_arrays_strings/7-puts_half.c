df12cbba4e6a418563d151a85bf45ff16bf0f4ac
Justin <justakinyi1@gmail.com>
2022-03-22 02:43:10 +0200

    j

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * puts - Prints a string
+ * @str: argument str to print
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	puts(str);
+}
