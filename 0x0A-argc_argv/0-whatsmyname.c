7588277d91e924c7cfb4710f1934ecf6e67ccd9a
Justin <justakinyi1@gmail.com>
2022-04-01 07:40:25 -0700

    arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/*
+ * main - passes arguments
+ *
+ * return; 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	_putchar("argc = %d/n", argc);
+		return (0);
+}
