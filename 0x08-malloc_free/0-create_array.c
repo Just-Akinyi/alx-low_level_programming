ee21f51b9ee431e66b17868c092691311c4891d3
Justin <justakinyi1@gmail.com>
2022-04-05 01:04:53 -0700

    memory allocation for array of char

---
+++ 0x08-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * create_array - creates array of chars
+ *
+ * @c - character to be allocated memory
+ * @size - size of character
+ *
+ * return - NULL(fail)
+ **/
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *s;
+	
+	if (size == 0)
+        {
+                return (NULL);
+        }
+	s = (char*)malloc(sizeof(char) * (size + 1));
+	
+	for (i = 0; i < size; i++)
+	{
+		s[i] = c;
+	}
+	return(s);
+}
