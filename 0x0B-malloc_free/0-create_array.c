3ceff7a69f004697792a3903e95db7d8033476c5
Justin <justakinyi1@gmail.com>
2022-04-05 01:21:13 -0700

    memory allocation for array of char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,25 +3,25 @@
 /**
  * create_array - creates array of chars
  *
- * @c - character to be allocated memory
- * @size - size of character
+ * @c - character to be allocated to array
+ * @size - size of array
  *
- * return - NULL(fail)
+ * return - array filled
  **/
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char *s;
-	
+
 	if (size == 0)
-        {
-                return (NULL);
-        }
-	s = (char*)malloc(sizeof(char) * (size + 1));
-	
+	{
+		return (NULL);
+	}
+	s = (char *)malloc(sizeof(char) * (size + 1));
+
 	for (i = 0; i < size; i++)
 	{
 		s[i] = c;
 	}
-	return(s);
+	return (s);
 }
