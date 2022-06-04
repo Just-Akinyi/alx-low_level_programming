77e433fa6af5cc2a08d7c36a964f6e75317480ec
Justin <justakinyi1@gmail.com>
2022-06-04 10:08:54 +0300

    ddd

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,2 @@
 #include <stdio.h>
-/**
- * main - prints a string
- * Return: 0 if exited properly, non-zero otherwise
- */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
+
