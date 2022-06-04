a048f50bd9e658f4428bc8ead7246c73bf09c612
Justin <justakinyi1@gmail.com>
2022-06-04 10:40:20 +0300

    C program that prints using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,4 @@
  * main - prints a string
  * Return: 0 if exited properly, non-zero otherwise
  */
-int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}
+
