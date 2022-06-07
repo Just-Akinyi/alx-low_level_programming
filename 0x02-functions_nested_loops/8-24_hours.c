80443ffac5e28b20a8daa5254863f7f5be233370
Justin <justakinyi1@gmail.com>
2022-06-07 13:03:45 +0300

    function that prints every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,9 @@
 
 /**
  * jack_bauer - Prints every minute of the day of
- *              Jack Bauer, starting from 00:00 to 23:59.
+ *
+ * Jack Bauer, starting from 00:00 to 23:59.
+ * Return: void
  */
 void jack_bauer(void)
 {
