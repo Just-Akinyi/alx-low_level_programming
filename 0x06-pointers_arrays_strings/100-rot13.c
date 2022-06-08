f30b807b202336ff15ad7c203d29033b46c1499e
Justin <justakinyi1@gmail.com>
2022-06-09 00:54:30 +0300

    function that encodes a string using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,10 +2,9 @@
 #include <stdio.h>
 
 /**
-  * rot13 - ...
-  * @s: ...
+  * rot13 - function that encodes a string using rot13
   *
-  * Return: ...
+  * Return: encoded string
   */
 char *rot13(char *s)
 {
