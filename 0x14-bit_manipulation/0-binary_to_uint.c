8ecfa214297fd629fd03bc6e2cc1719315cd3ed2
Just-Akinyi <justakinyi1@gmail.com>
2022-08-20 08:59:25 +0300

    converts a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,25 +1,64 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: A pointer to a string of 0 and 1 chars.
- *
- * Return: If b is NULL or contains chars not 0 or 1 - 0.
- *         Otherwise - the converted number.
+ * _strlen- calcul la taile d'un string
+ * @b: string
+ * Return: taile
  */
+int _strlen(const char *b)
+{
+	int len = 0;
+
+	while (*b)
+	{
+		b++;
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * _pow - calcule la puissance d'un nombre 2 par défaut
+ * @puissance: la puissance
+ * Return: int
+ */
+unsigned int _pow(int puissance)
+{
+	if (puissance == 0)
+		return (1);
+
+	return (2 * _pow(puissance - 1));
+}
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: pointer to a string of 0 and 1 chars
+ * Return: converted number or 0
+ */
+
 unsigned int binary_to_uint(const char *b)
 {
-  unsigned int val = 0;
-  int i = 0;
+	int i = 0, len;
+	unsigned int converted_num = 0;
 
-  if (b == NULL)
-    return 0;
+	if (b == NULL)
+		return (0);
 
-  while (b[i] == '0' || b[i] == '1')
-  {  // Found another digit.
-    val <<= 1;
-    val += b[i]-'0';
-    i++;
-  }
-  return val;
-}
+	len = _strlen(b);
+
+	while (*b)
+	{
+		if ((*b != '0' && *b != '1'))
+		{
+			return (0);
+		}
+
+		if (*b == '1')
+			converted_num = converted_num + (_pow(len - 1));
+		i++;
+		b++;
+		len--;
+	}
+
+	return (converted_num);
+
